d7ef42d404282ea39d3d8b2ad8c6cd7a082a6487
virgiliojosue <65056259+virgiliojosue@users.noreply.github.com>
2020-05-20 22:27:11 -0600

    Correccion funcion str() y agregado de funcion mul()

--- Codigo.py
+++ Codigo.py
@@ -218,7 +218,7 @@ class Codigo:
 
             if len(registros) == 2:
 
-                if int(registros[0][1:]) >-1 and int(registros[0][1:]) < 12 and int(registros[1][1:]) >-1 and int(registros[1][1:]) < 12:
+                if int(registros[0][1:]) >-1 and int(registros[0][1:]) < 13 and int(registros[1][1:]) >-1 and int(registros[1][1:]) < 13:
        	 		
                     valorregistro = self.registro[registros[0]]
                     direccionRam = self.registro[registros[1]]
@@ -230,13 +230,13 @@ class Codigo:
 
                 else:
                     
-                    self.registro["error"] = 5
+                    self.registro["error"] = 12
                     self.registro["descrError"] = "El valor del registro no es valido"
                     self.registro["lineaError"] = self.obtener_llave(line,self.codigo)
           	 
             elif len(registros)==3 and registros[2][:1] == "#":
 
-                if int(registros[0][1:]) >-1 and int(registros[0][1:]) < 12 and int(registros[1][1:])>-1 and int(registros[1][1:]) <12:
+                if int(registros[0][1:]) >-1 and int(registros[0][1:]) < 13 and int(registros[1][1:])>-1 and int(registros[1][1:]) <13:
           		
                     if int(registros[2][1:])%4==0 and int(registros[2][1:]) < 36:
             		
@@ -251,20 +251,20 @@ class Codigo:
 
                     else:
                     
-                        self.registro["error"] = 5
+                        self.registro["error"] = 13
                         self.registro["descrError"] = "El valor del offset no es valido"
                         self.registro["lineaError"] = self.obtener_llave(line,self.codigo)
 
 
                 else: 
           		
-                    self.registro["error"] = 5
+                    self.registro["error"] = 12
                     self.registro["descrError"] = "El valor del registro no es valido"
                     self.registro["lineaError"] = self.obtener_llave(line,self.codigo)
         # verifica si la instruccion es del tipo rb, [rc,rd]
             elif len(registros)==3 and registros[2][:1] == "r":
             
-                if int(registros[0][1:]) >-1 and int(registros[0][1:]) < 12 and int(registros[1][1:])>-1 and int(registros[1][1:]) <12 and int(registros[2][1:])>-1 and int(registros[2][1:])<12:
+                if int(registros[0][1:]) >-1 and int(registros[0][1:]) < 13 and int(registros[1][1:])>-1 and int(registros[1][1:]) <13 and int(registros[2][1:])>-1 and int(registros[2][1:])<13:
                 
                     valorregistro1= int(self.registro[registros[2]],16)
 
@@ -280,13 +280,13 @@ class Codigo:
 
                     else:
 
-                        self.registro["error"] = 5
-                        self.registro["descrError"] = "El valor del offset no es valido"
+                        self.registro["error"] = 14
+                        self.registro["descrError"] = "El valor contenido en el tercer registro no es valido"
                         self.registro["lineaError"] = self.obtener_llave(line,self.codigo)
             
                 else:
 
-                    self.registro["error"] = 5
+                    self.registro["error"] = 12
                     self.registro["descrError"] = "El valor del registro no es valido"
                     self.registro["lineaError"] = self.obtener_llave(line,self.codigo)
 
@@ -295,6 +295,62 @@ class Codigo:
                 self.registro["error"] = 4
                 self.registro["descrError"] = "Error de sintaxis"
                 self.registro["lineaError"] = self.obtener_llave(line,self.codigo)
+
+    def mul(self,line):
+    if re.search("^mul\s+r\d{1,2}\s*,\s*(r\d{1,2}\s*|r\d{1,2}\s*,\s*r\d{1,2}\s*)?$", line) != None:
+       
+        registros = re.findall("r\d{1,2}",line)
+
+        if len(registros)==2:
+
+            if int(registros[0][1:]) >-1 and int(registros[0][1:]) < 13 and int(registros[1][1:]) >-1 and int(registros[1][1:]) < 13:
+
+                valorRegisDestino= int(self.registro[registros[0]],16)  
+                valorRegisFuente= int(self.registro[registros[1]],16)
+                valorMultiplicacion= valorRegisFuente*valorRegisDestino
+
+                self.registro[registros[0]]= '0x{0:0{1}X}'.format(valorMultiplicacion,8)
+
+            else:
+                self.registro["error"] = 12
+                self.registro["descrError"] = "El valor del registro no es valido"
+          	    self.registro["lineaError"] = self.obtener_llave(line,self.codigo)
+
+        if len(registros)==3:
+
+            if int(registros[0][1:]) >-1 and int(registros[0][1:]) < 13 and int(registros[1][1:]) >-1 and int(registros[1][1:]) < 13 and int(registros[2][1:])> -1 and int(registros[2][1:])< 13:
+
+                if registros[0] == registros[1]:
+                    
+                    valorRegisDestino= int(self.registro[registros[1]],16)  
+                    valorRegisFuente= int(self.registro[registros[2]],16])
+                    valorMultiplicacion= valorRegisFuente*valorRegisDestino
+
+                    self.registro[registros[0]]= '0x{0:0{1}X}'.format(valorMultiplicacion,8)
+
+                elif registros[0] == registros[2]:
+
+                    valorRegisDestino= int(self.registro[registros[1]],16)  
+                    valorRegisFuente= int(self.registro[registros[2]],16])
+                    valorMultiplicacion= valorRegisFuente*valorRegisDestino
+
+                    self.registro[registros[0]]= '0x{0:0{1}X}'.format(valorMultiplicacion,8)
+
+                else: 
+                    self.registro["error"] = 15
+                    self.registro["descrError"] = "Debe introducir dos registros iguales"
+          	        self.registro["lineaError"] = self.obtener_llave(line,self.codigo)
+
+            else:
+                self.registro["error"] = 12
+                self.registro["descrError"] = "El valor del registro no es valido"
+          	    self.registro["lineaError"] = self.obtener_llave(line,self.codigo)
+
+        else:
+            self.registro["error"] = 4
+            self.registro["descrError"] = "Error de sintaxis"
+          	self.registro["lineaError"] = self.obtener_llave(line,self.codigo)
+
 
     def obtener_direccion(self,valor = None):
         return hex(537329664 + list(self.ram.values()).index("0x00")) if valor == None else hex(537329664 + list(self.ram.values()).index(valor))
